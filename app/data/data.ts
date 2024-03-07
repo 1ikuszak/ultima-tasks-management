55715cf87218dbfa19289673aafa3afb013411d0
1ikuszak <luke.glica@gmail.com>
2024-03-07 11:01:15 +0100

    chore: removed unnecessary

--- app/data/data.ts
+++ app/data/data.ts
@@ -1,62 +1,7 @@
-// Define the structure of a color theme
-export type ColorThemeVariant = {
-  primary: string
-  secondary: string
-  bg: string
-}
-
-// Enum for color keys
-export enum ColorKey {
-  default = 'default',
-  blue = 'blue',
-  green = 'green',
-  rose = 'rose',
-  orange = 'orange',
-  // Add other keys as needed
-}
-
-// Type for the mapping between ColorKey and Tailwind class names
-export type ColorMap = {
-  [key in ColorKey]?: ColorThemeVariant
-}
-
-// Color mapping
-export const colorMap: ColorMap = {
-  blue: {
-    primary: 'theme-blue-primary',
-    secondary: 'theme-blue-secondary',
-    bg: 'theme-blue-bg',
-  },
-  green: {
-    primary: 'theme-green-primary',
-    secondary: 'theme-green-secondary',
-    bg: 'theme-green-bg',
-  },
-  rose: {
-    primary: 'theme-rose-primary',
-    secondary: 'theme-rose-secondary',
-    bg: 'theme-rose-bg',
-  },
-  default: {
-    primary: 'theme-default-primary',
-    secondary: 'theme-default-secondary',
-    bg: 'theme-default-bg',
-  },
-  orange: {
-    primary: 'theme-orange-primary',
-    secondary: 'theme-orange-secondary',
-    bg: 'theme-orange-bg',
-  },
-}
-
 import {
   ArrowDownIcon,
   ArrowRightIcon,
   ArrowUpIcon,
-  CheckCircledIcon,
-  CircleIcon,
-  CrossCircledIcon,
-  StopwatchIcon,
 } from '@radix-ui/react-icons'
 
 import {
